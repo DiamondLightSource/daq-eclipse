2406b8a9bf270eaa20fcc1c510981b91c3033b8c
Matthew Gerring <matthew.gerring@diamond.ac.uk>
2015-12-15 14:04:35 +0000

    Added exception

---
+++ org.eclipse.scanning.api/src/org/eclipse/scanning/api/scan/ParsingException.java
@@ -0,0 +1,31 @@
+package org.eclipse.scanning.api.scan;
+
+public class ParsingException extends Exception {
+
+	public ParsingException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParsingException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParsingException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParsingException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParsingException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
