4e27c4f3a623e02edb653e339f8f5f4bf48ba5b4
Matthew Dickie <matthew.dickie@diamond.ac.uk>
2015-12-16 17:42:35 +0000

    [Nexus Scan] added rank to ScannableModel

--- org.eclipse.scanning.api/src/org/eclipse/scanning/api/ScannableModel.java
+++ org.eclipse.scanning.api/src/org/eclipse/scanning/api/ScannableModel.java
@@ -1,13 +1,23 @@
 package org.eclipse.scanning.api;
 
 /**
- * 
+ *
  * This class contains the data required by a scannable when it
  * is configured during a scan. For instance, information for writing a NeXus file.
- * 
+ *
  * @author Matthew Gerring
  *
  */
 public class ScannableModel {
+
+	private int rank;
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	public int getRank() {
+		return rank;
+	}
 
 }
