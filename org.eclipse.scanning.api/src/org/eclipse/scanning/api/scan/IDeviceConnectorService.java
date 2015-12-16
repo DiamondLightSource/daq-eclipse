3056e50b860e37f031004a936505fae144e47c20
James Mudd <james.mudd@diamond.ac.uk>
2015-12-16 13:44:53 +0000

    Improvements to JavaDoc

--- org.eclipse.scanning.api/src/org/eclipse/scanning/api/scan/IDeviceConnectorService.java
+++ org.eclipse.scanning.api/src/org/eclipse/scanning/api/scan/IDeviceConnectorService.java
@@ -5,23 +5,23 @@ import org.eclipse.scanning.api.IScannable;
 /**
  * 
  * Clients do not need to consume this service, it is used
- * to provide connection to devices which already exist, like those in GFA8.
- * 
+ * to provide connection to devices which already exist, like those in GDA8.
+ * <p>
  * A bundle implements this service and the scanning
  * consumes the service. The tests provide the service
  * directly.
- * 
+ * <p>
  * This service is designed to be implemented by GDA8 to 
  * provide scannables (existing ones) to the new system.
  * This can be done easily as IScannable is a subset of
  * Scannable in GDA8.
- * 
+ * <p>
  * It also provides IDetectors. These are not analogous but
  * not the same as Detector in GDA. The service wraps both gda.device.Detector
  * into IReadableDetector and gda.px.detector.Detector into IScanner. In the
  * first case the model provides the collection time and anything else required.
  * In the later case the model provides the file path and omegaStart required.
- * 
+ * <p>
  * It mirrors the other connector services like the jms one,
  * which push the details of getting connections outside the
  * core scanning API using declarative services.
