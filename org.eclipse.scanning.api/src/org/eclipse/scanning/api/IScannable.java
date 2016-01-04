22f464977467577e8109c33bbe5d9e233e713d69
Matthew Gerring <matthew.gerring@diamond.ac.uk>
2016-01-04 11:34:32 +0000

    Made scannable not configurable again as we send all the positions during the scan.

--- org.eclipse.scanning.api/src/org/eclipse/scanning/api/IScannable.java
+++ org.eclipse.scanning.api/src/org/eclipse/scanning/api/IScannable.java
@@ -12,7 +12,7 @@ import org.eclipse.scanning.api.points.IPosition;
  * @author fcp94556
  *
  */
-public interface IScannable<T> extends ILevel, INameable, IConfigurable<ScannableModel> {
+public interface IScannable<T> extends ILevel, INameable {
 	
 	/**
 	 * Returns the current position of the Scannable. Called by ConcurentScan at the end of the point. 
--- org.eclipse.scanning.test/src/org/eclipse/scanning/test/scan/mock/MockScannable.java
+++ org.eclipse.scanning.test/src/org/eclipse/scanning/test/scan/mock/MockScannable.java
@@ -1,9 +1,7 @@
 package org.eclipse.scanning.test.scan.mock;
 
 import org.eclipse.scanning.api.IScannable;
-import org.eclipse.scanning.api.ScannableModel;
 import org.eclipse.scanning.api.points.IPosition;
-import org.eclipse.scanning.api.scan.ScanningException;
 
 public class MockScannable implements IScannable<Number> {
 
@@ -11,8 +9,6 @@ public class MockScannable implements IScannable<Number> {
 	private String  name;
 	private Number  position = 0d;
 	private boolean requireSleep=true;
-	private ScannableModel model;
-
 	
     public MockScannable() {
     	
@@ -32,12 +28,6 @@ public class MockScannable implements IScannable<Number> {
 		this.name = name;
 		this.position = position;
 	}
-	
-	@Override
-	public void configure(ScannableModel model) throws ScanningException {
-		this.model = model;
-	}
-
 	
 	public int getLevel() {
 		return level;
