f9bdad0d1073de548a1c7847133db0d0a93e163d
Colin Palmer <colin.palmer@diamond.ac.uk>
2016-01-15 11:58:25 +0000

    Unify JSONMarshallerTests now the OSGi type resolver is always used

--- uk.ac.diamond.daq.activemq.connector.test/src/uk/ac/diamond/daq/activemq/connector/JSONMarshallerTest.java
+++ uk.ac.diamond.daq.activemq.connector.test/src/uk/ac/diamond/daq/activemq/connector/JSONMarshallerTest.java
@@ -1,6 +1,8 @@
 package uk.ac.diamond.daq.activemq.connector;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 import static org.hamcrest.CoreMatchers.*;
 
 import java.util.Arrays;
@@ -14,7 +16,15 @@ import org.apache.commons.lang.StringEscapeUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Version;
 
+import uk.ac.diamond.daq.activemq.connector.internal.Activator;
+import uk.ac.diamond.daq.activemq.connector.internal.MockClassLoaderAnswer;
+import uk.ac.diamond.daq.activemq.connector.internal.TestBundleProvider;
 import uk.ac.diamond.daq.activemq.connector.testobject.Animal;
 import uk.ac.diamond.daq.activemq.connector.testobject.Bird;
 import uk.ac.diamond.daq.activemq.connector.testobject.Cat;
@@ -32,21 +42,21 @@ import uk.ac.diamond.daq.activemq.connector.testobject.Person;
  * @author Colin Palmer
  *
  */
-public abstract class JSONMarshallerTestBase {
+public class JSONMarshallerTest {
 
-	protected String jsonForJim;
-	protected String jsonForJohn;
-	protected String jsonForFelix;
-	protected String jsonForAnimalArray;
-	protected String jsonForAnimalList;
-	protected String jsonForAnimalSet;
-	protected String jsonForAnimalMap;
+	protected String jsonForJim = "{\n  \"@bundle_and_class\" : \"bundle=uk.ac.diamond.daq.test.example&version=1.2.0.test&class=uk.ac.diamond.daq.activemq.connector.testobject.Person\",\n  \"name\" : \"Jim\",\n  \"pet\" : {\n    \"@bundle_and_class\" : \"bundle=uk.ac.diamond.daq.test.example&version=2.0.0&class=uk.ac.diamond.daq.activemq.connector.testobject.Bird\",\n    \"name\" : \"Polly\",\n    \"feathers\" : \"Green\"\n  }\n}";
+	protected String jsonForJohn = "{\n  \"@bundle_and_class\" : \"bundle=uk.ac.diamond.daq.test.example&version=1.2.0.test&class=uk.ac.diamond.daq.activemq.connector.testobject.Person\",\n  \"name\" : \"John\",\n  \"pet\" : {\n    \"@bundle_and_class\" : \"bundle=uk.ac.diamond.daq.test.other_example&version=0.0.0&class=uk.ac.diamond.daq.activemq.connector.testobject.Cat\",\n    \"name\" : \"Felix\",\n    \"whiskers\" : \"Luxuriant\"\n  }\n}";
+	protected String jsonForFelix = "{\n  \"@bundle_and_class\" : \"bundle=uk.ac.diamond.daq.test.other_example&version=0.0.0&class=uk.ac.diamond.daq.activemq.connector.testobject.Cat\",\n  \"name\" : \"Felix\",\n  \"whiskers\" : \"Luxuriant\"\n}";
+	protected String jsonForAnimalArray = "[ \"bundle=&version=&class=[Luk.ac.diamond.daq.activemq.connector.testobject.Animal;\", [ {\n  \"@bundle_and_class\" : \"bundle=uk.ac.diamond.daq.test.other_example&version=0.0.0&class=uk.ac.diamond.daq.activemq.connector.testobject.Cat\",\n  \"name\" : \"Felix\",\n  \"whiskers\" : \"Luxuriant\"\n}, {\n  \"@bundle_and_class\" : \"bundle=uk.ac.diamond.daq.test.example&version=2.0.0&class=uk.ac.diamond.daq.activemq.connector.testobject.Bird\",\n  \"name\" : \"Polly\",\n  \"feathers\" : \"Green\"\n}, {\n  \"@bundle_and_class\" : \"bundle=uk.ac.diamond.daq.test.other_example&version=0.0.0&class=uk.ac.diamond.daq.activemq.connector.testobject.Cat\",\n  \"name\" : \"Felix\",\n  \"whiskers\" : \"Luxuriant\"\n} ] ]";
+	protected String jsonForAnimalList = "[ \"bundle=&version=&class=java.util.ArrayList\", [ {\n  \"@bundle_and_class\" : \"bundle=uk.ac.diamond.daq.test.other_example&version=0.0.0&class=uk.ac.diamond.daq.activemq.connector.testobject.Cat\",\n  \"name\" : \"Felix\",\n  \"whiskers\" : \"Luxuriant\"\n}, {\n  \"@bundle_and_class\" : \"bundle=uk.ac.diamond.daq.test.example&version=2.0.0&class=uk.ac.diamond.daq.activemq.connector.testobject.Bird\",\n  \"name\" : \"Polly\",\n  \"feathers\" : \"Green\"\n}, {\n  \"@bundle_and_class\" : \"bundle=uk.ac.diamond.daq.test.other_example&version=0.0.0&class=uk.ac.diamond.daq.activemq.connector.testobject.Cat\",\n  \"name\" : \"Felix\",\n  \"whiskers\" : \"Luxuriant\"\n} ] ]";
+	protected String jsonForAnimalSet = "[ \"bundle=&version=&class=java.util.HashSet\", [ {\n  \"@bundle_and_class\" : \"bundle=uk.ac.diamond.daq.test.other_example&version=0.0.0&class=uk.ac.diamond.daq.activemq.connector.testobject.Cat\",\n  \"name\" : \"Felix\",\n  \"whiskers\" : \"Luxuriant\"\n}, {\n  \"@bundle_and_class\" : \"bundle=uk.ac.diamond.daq.test.example&version=2.0.0&class=uk.ac.diamond.daq.activemq.connector.testobject.Bird\",\n  \"name\" : \"Polly\",\n  \"feathers\" : \"Green\"\n} ] ]";
+	protected String jsonForAnimalMap = "{\n  \"@bundle_and_class\" : \"bundle=&version=&class=java.util.HashMap\",\n  \"Polly\" : {\n    \"@bundle_and_class\" : \"bundle=uk.ac.diamond.daq.test.example&version=2.0.0&class=uk.ac.diamond.daq.activemq.connector.testobject.Bird\",\n    \"name\" : \"Polly\",\n    \"feathers\" : \"Green\"\n  },\n  \"Felix\" : {\n    \"@bundle_and_class\" : \"bundle=uk.ac.diamond.daq.test.other_example&version=0.0.0&class=uk.ac.diamond.daq.activemq.connector.testobject.Cat\",\n    \"name\" : \"Felix\",\n    \"whiskers\" : \"Luxuriant\"\n  },\n  \"John\" : {\n    \"@bundle_and_class\" : \"bundle=uk.ac.diamond.daq.test.example&version=1.2.0.test&class=uk.ac.diamond.daq.activemq.connector.testobject.Person\",\n    \"name\" : \"John\",\n    \"pet\" : {\n      \"@bundle_and_class\" : \"bundle=uk.ac.diamond.daq.test.other_example&version=0.0.0&class=uk.ac.diamond.daq.activemq.connector.testobject.Cat\",\n      \"name\" : \"Felix\",\n      \"whiskers\" : \"Luxuriant\"\n    }\n  },\n  \"Jim\" : {\n    \"@bundle_and_class\" : \"bundle=uk.ac.diamond.daq.test.example&version=1.2.0.test&class=uk.ac.diamond.daq.activemq.connector.testobject.Person\",\n    \"name\" : \"Jim\",\n    \"pet\" : {\n      \"@bundle_and_class\" : \"bundle=uk.ac.diamond.daq.test.example&version=2.0.0&class=uk.ac.diamond.daq.activemq.connector.testobject.Bird\",\n      \"name\" : \"Polly\",\n      \"feathers\" : \"Green\"\n    }\n  }\n}";
 	protected String testString = "Hello world!";
 	protected String jsonForTestString = "\"Hello world!\"";
 	protected int testInt = -56;
 	protected String jsonForTestInt = "-56";
 	protected long testLong = 1234567890L;
-	protected String jsonForTestLong = "1234567890";
+	protected String jsonForTestLong = "[ \"bundle=&version=&class=java.lang.Long\", 1234567890 ]";
 
 	protected ActivemqConnectorService marshaller;
 
@@ -58,8 +68,43 @@ public abstract class JSONMarshallerTestBase {
 	private Person jim;
 	private Person john;
 
+	// Mocks
+	@Mock private Bundle exampleBundleV1;
+	@Mock private Bundle exampleBundleV2;
+	@Mock private Bundle otherExampleBundle;
+	@Mock private BundleContext bundleContext;
+
 	@Before
-	public void createTestObjects() throws Exception {
+	public void setUp() throws Exception {
+		createTestObjects();
+		MockitoAnnotations.initMocks(this);
+
+		when(exampleBundleV1.getSymbolicName()).thenReturn(Person.BUNDLE_NAME_FOR_TESTING);
+		when(exampleBundleV1.getVersion()).thenReturn(new Version(Person.BUNDLE_VERSION_FOR_TESTING));
+		MockClassLoaderAnswer exampleV1Answer = new MockClassLoaderAnswer(Person.class, Animal.class);
+		when(exampleBundleV1.loadClass(any())).thenAnswer(exampleV1Answer);
+
+		when(exampleBundleV2.getSymbolicName()).thenReturn(Bird.BUNDLE_NAME_FOR_TESTING);
+		when(exampleBundleV2.getVersion()).thenReturn(new Version(Bird.BUNDLE_VERSION_FOR_TESTING));
+		when(exampleBundleV2.loadClass(any())).thenAnswer(new MockClassLoaderAnswer(Bird.class));
+
+		when(otherExampleBundle.getSymbolicName()).thenReturn(Cat.BUNDLE_NAME_FOR_TESTING);
+		when(otherExampleBundle.getVersion()).thenReturn(Version.emptyVersion);
+		when(otherExampleBundle.loadClass(any())).thenAnswer(new MockClassLoaderAnswer(Cat.class));
+
+		when(bundleContext.getBundles()).thenReturn(new Bundle[] { exampleBundleV1, exampleBundleV2, otherExampleBundle });
+		new Activator().start(bundleContext);
+
+		TestBundleProvider bundleProvider = new TestBundleProvider();
+		bundleProvider.registerBundleForClass(Person.class, exampleBundleV1);
+		bundleProvider.registerBundleForClass(Animal.class, exampleBundleV1);
+		bundleProvider.registerBundleForClass(Bird.class, exampleBundleV2);
+		bundleProvider.registerBundleForClass(Cat.class, otherExampleBundle);
+
+		marshaller = new ActivemqConnectorService(bundleProvider);
+	}
+
+	private void createTestObjects() {
 
 		polly = new Bird();
 		polly.setName("Polly");
@@ -82,7 +127,7 @@ public abstract class JSONMarshallerTestBase {
 	public void tearDown() throws Exception {
 		if (json != null) {
 			// So we can see what's going on
-			System.out.println("JSON: " + json);
+//			System.out.println("JSON: " + json);
 
 			// To make it easy to replace expected JSON values in the code when we're sure they're correct
 			@SuppressWarnings("unused")
