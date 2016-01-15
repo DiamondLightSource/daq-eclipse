4027f28eafbbb16f757c0b092cd915127d84ab27
Colin Palmer <colin.palmer@diamond.ac.uk>
2016-01-15 15:52:02 +0000

    Remove TODO comment

--- org.eclipse.scanning.event/src/org/eclipse/scanning/event/PublisherImpl.java
+++ org.eclipse.scanning.event/src/org/eclipse/scanning/event/PublisherImpl.java
@@ -213,9 +213,6 @@ class PublisherImpl<T> extends AbstractConnection implements IPublisher<T> {
 			if (m instanceof TextMessage) {
 				TextMessage t = (TextMessage)m;
 
-				// TODO this currently fails when doing a dry run
-				// ScanBeans are received as TextMessages but this attempts to deserialize them as MappingExperimentStatusBeans
-				// Need to check queues and topics are set up correctly
 				final Object qbean = service.unmarshal(t.getText(), null);
 				if (qbean==null) continue;
 				if (isSame(qbean, bean)) {
