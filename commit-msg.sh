1507cf0afcfbd3a090d1e6d3753155b5499726bc
Matthew Gerring <matthew.gerring@gmail.com>
2016-02-05 09:52:03 +0000

    Update commit-msg.sh
    http://jira.diamond.ac.uk/browse/DATAACQTEAM-223

--- commit-msg.sh
+++ commit-msg.sh
@@ -4,6 +4,7 @@
 commit_regex='(jira\.diamond\.ac\.uk|merge)'
 error_msg="Aborting commit. Your commit message must reference a jira ticket or be 'Merge'"
 
+# We check the last commit message has a ticket
 if ! grep -iqE "$commit_regex" "./git/COMMIT_EDITMSG"; then
     echo "$error_msg" >&2
     exit 1
