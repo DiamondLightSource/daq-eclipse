00c5f9938f05c90c825424e80fd2e95bb83725a9
Matthew Gerring <matthew.gerring@diamond.ac.uk>
2016-02-05 12:39:55 +0000

    Update script which checks commits
    http://jira.diamond.ac.uk/browse/DATAACQTEAM-223

--- org.eclipse.scanning.releng/build/commit-msg.sh
+++ org.eclipse.scanning.releng/build/commit-msg.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
 
 # regex to validate in commit msg
-commit_regex='(jira\.diamond\.ac\.uk|merge)'
-error_msg="Aborting commit. Your commit message must reference a jira ticket or be 'Merge'"
+commit_regex="(jira\.diamond\.ac\.uk|merge)"
 
-last_msg = $(git log -1 --pretty=%B)
+
+last_msg="$(git log -1 --pretty=%B)"
+echo "The last commit was: $last_msg"
+
 
 # We check the last commit message has a ticket
-if [[ "$last_msg" =~ "$commit_regex" ]]; then
+if [[ $last_msg =~ .*(jira\.diamond\.ac\.uk|merge).* ]]; then
+    echo "Jira ticket or merge found, build may proceed"
     exit 0
 fi
 
 # Couldn't find ticket or merge, fail build
+error_msg="Aborting build. Your commit message must reference a jira ticket or be 'Merge'"
 echo "$error_msg" >&2
+
+# Get the last user
+id="$(git log --format="%H" -n 1)"
+user="$(git show --format="%aN <%aE>" $id)"
+echo "Last commit from:"
+echo " $user"
+
 exit 1
