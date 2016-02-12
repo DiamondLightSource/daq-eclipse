c51189511d2f818131dc64a1c5f34931967f5d46
Richard Fearn <richard.fearn@diamond.ac.uk>
2016-02-12 12:44:20 +0000

    Clean up EventException
    Mainly, use the same argument names as in Exception.

--- org.eclipse.scanning.api/src/org/eclipse/scanning/api/event/EventException.java
+++ org.eclipse.scanning.api/src/org/eclipse/scanning/api/event/EventException.java
@@ -2,29 +2,26 @@ package org.eclipse.scanning.api.event;
 
 public class EventException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -6644630244716886256L;
 
 	public EventException() {
 		super();
 	}
 
-	public EventException(String arg0, Throwable arg1, boolean arg2,
-			boolean arg3) {
-		super(arg0, arg1, arg2, arg3);
+	public EventException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
 	}
 
-	public EventException(String arg0, Throwable arg1) {
-		super(arg0, arg1);
+	public EventException(String message, Throwable throwable) {
+		super(message, throwable);
 	}
 
-	public EventException(String arg0) {
-		super(arg0);
+	public EventException(String message) {
+		super(message);
 	}
 
-	public EventException(Throwable arg0) {
-		super(arg0);	}
+	public EventException(Throwable throwable) {
+		super(throwable);
+	}
 
 }
