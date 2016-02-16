f3fad4d09fb48f05e5accb8e83d4146726d5556d
Matthew Gerring <matthew.gerring@diamond.ac.uk>
2016-02-16 15:38:53 +0000

    Import clean up
    http://jira.diamond.ac.uk/browse/DATAACQTEAM-197

--- org.eclipse.scanning.test/src/org/eclipse/scanning/test/scan/nexus/MonitorPluginTest.java
+++ org.eclipse.scanning.test/src/org/eclipse/scanning/test/scan/nexus/MonitorPluginTest.java
@@ -2,8 +2,8 @@ package org.eclipse.scanning.test.scan.nexus;
 
 import static org.eclipse.dawnsci.nexus.test.util.NexusAssert.assertAxes;
 import static org.eclipse.dawnsci.nexus.test.util.NexusAssert.assertIndices;
-import static org.eclipse.dawnsci.nexus.test.util.NexusAssert.assertSignal;
 import static org.eclipse.dawnsci.nexus.test.util.NexusAssert.assertTarget;
+import static org.eclipse.dawnsci.nexus.test.util.NexusAssert.assertSignal;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
