ffbff12e9c62b51dbb624ceaef7555392871c292
Colin Palmer <colin.palmer@diamond.ac.uk>
2016-02-22 13:42:45 +0000

    Update tests
    http://jira.diamond.ac.uk/browse/DATAACQTEAM-222

--- uk.ac.diamond.json.test/src/uk/ac/diamond/json/test/JSONUnmarshallingRegressionTest.java
+++ uk.ac.diamond.json.test/src/uk/ac/diamond/json/test/JSONUnmarshallingRegressionTest.java
@@ -34,7 +34,7 @@ public class JSONUnmarshallingRegressionTest {
 	// An example of a bean used by Xia2 which could be sent by another process and must deserialize correctly in current version
 	private static final String JSON_FOR_PROJECT_BEAN = "{\"status\":\"COMPLETE\",\"name\":\"X1_weak_M1S1_1 - X1_weak_M1S1_1\",\"message\":\"Xia2 run completed normally\",\"percentComplete\":100.0,\"userName\":\"awa25\",\"hostName\":\"cs04r-sc-vserv-45.diamond.ac.uk\",\"runDirectory\":\"/dls/i03/data/2016/cm14451-1/processed/tmp/2016-01-27/fake085224/MultiCrystal_1\",\"uniqueId\":\"1453910139320_94ed2a2b-997e-4dbc-ad6e-0c3c04bb2c82\",\"submissionTime\":1453910139340,\"properties\":null,\"projectName\":\"MultiCrystalRerun\",\"cystalName\":\"fake085224\",\"sweeps\":[{\"name\":\"X1_weak_M1S1_1\",\"sessionId\":\"55167\",\"dataCollectionId\":\"1007379\",\"imageDirectory\":\"/dls/i03/data/2016/cm14451-1/tmp/2016-01-27/fake085224/\",\"firstImageName\":\"X1_weak_M1S1_1_0001.cbf\",\"start\":1,\"end\":900,\"wavelength\":0.979493,\"xBeam\":212.51,\"yBeam\":219.98,\"resolution\":null}],\"wavelength\":\"NaN\",\"commandLineSwitches\":\"\",\"anomalous\":true,\"spaceGroup\":null,\"unitCell\":null,\"resolution\":null}";
 
-	// Example of JSON produced for an ROI. (Encoded object should really be a ROIBean but is actually a String)
+	// Example of JSON produced for an ROI
 	private static final String JSON_FOR_RECTANGULAR_ROI = "{\n  \"@bundle_and_class\" : \"bundle=&version=&class=org.eclipse.dawnsci.analysis.dataset.roi.json.RectangularROIBean\",\n  \"type\" : \"RectangularROI\",\n  \"startPoint\" : [ -3.5, 4.0 ],\n  \"lengths\" : [ 8.0, 6.1 ],\n  \"angle\" : 0.0,\n  \"endPoint\" : [ 4.5, 10.1 ]\n}";
 
 	private static final String JSON_FOR_WRAPPED_RECTANGULAR_ROI = "{\n  \"@bundle_and_class\" : \"bundle=&version=&class=uk.ac.diamond.json.test.ROIWrapper\",\n  \"object\" : {\n    \"@bundle_and_class\" : \"bundle=&version=&class=org.eclipse.dawnsci.analysis.dataset.roi.json.RectangularROIBean\",\n    \"type\" : \"RectangularROI\",\n    \"startPoint\" : [ -3.5, 4.0 ],\n    \"lengths\" : [ 8.0, 6.1 ],\n    \"angle\" : 0.0,\n    \"endPoint\" : [ 4.5, 10.1 ]\n  }\n}";
@@ -129,7 +129,7 @@ public class JSONUnmarshallingRegressionTest {
 	public void testGenericROIFieldDeserialization() throws Exception {
 		IROI roi = new RectangularROI(-3.5, 4.0, 8.0, 6.1, 0.0);
 		ObjectWrapper<IROI> expected = new ObjectWrapper<>(roi);
-		ObjectWrapper<?> actual = marshaller.unmarshal(JSON_FOR_WRAPPED_RECTANGULAR_ROI, ObjectWrapper.class);
+		ObjectWrapper<?> actual = marshaller.unmarshal(JSON_FOR_GENERIC_WRAPPED_RECTANGULAR_ROI, ObjectWrapper.class);
 		assertEquals(expected, actual);
 	}
 
