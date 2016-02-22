c34ce32438ae16e8897674ba3242f8743c797dc6
Nic Bricknell <nic.bricknell@diamond.ac.uk>
2016-02-22 10:44:02 +0000

    Fix StepGenerator.size() (but not .createPoints()) for misdirected steps
    http://jira.diamond.ac.uk/browse/DATAACQTEAM-234

--- org.eclipse.scanning.points/src/org/eclipse/scanning/points/StepGenerator.java
+++ org.eclipse.scanning.points/src/org/eclipse/scanning/points/StepGenerator.java
@@ -18,6 +18,7 @@ public class StepGenerator extends AbstractGenerator<StepModel, IPosition> {
 	public int size() throws GeneratorException {
 		if (containers!=null) throw new GeneratorException("Cannot deal with regions in a step scan!");
 		double div = ((model.getStop()-model.getStart())/model.getStep());
+		if (div < 0) throw new GeneratorException("Model step size has the wrong sign!");
 		div += (model.getStep() / 100); // add tolerance of 1% of step value
 		return (int)Math.floor(div+1);
 	}
