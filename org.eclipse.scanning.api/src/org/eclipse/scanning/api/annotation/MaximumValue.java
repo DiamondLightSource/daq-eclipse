8d083eeca7fb84293ec3b16bcc86b98ff174599a
Colin Palmer <colin.palmer@diamond.ac.uk>
2016-03-08 12:03:40 +0000

    Add MinimumValue, MaximumValue and Units annotations
    (copied from org.eclipse.richbeans.generator)
    
    http://jira.diamond.ac.uk/browse/DATAACQTEAM-220

---
+++ org.eclipse.scanning.api/src/org/eclipse/scanning/api/annotation/MaximumValue.java
@@ -0,0 +1,18 @@
+package org.eclipse.scanning.api.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * Specify the maximum value a numeric field should contain.
+ *
+ * @author James Mudd
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface MaximumValue {
+	String value();
+}
