34c4c4cf83eef09cfbb4894837fded0eeba18f30
Nic Bricknell <nic.bricknell@diamond.ac.uk>
2016-03-09 09:41:23 +0000

    Add some docstrings for Python scanning syntax
    http://jira.diamond.ac.uk/browse/DAQ-2

--- org.eclipse.scanning.command/python/scan_syntax.py
+++ org.eclipse.scanning.command/python/scan_syntax.py
@@ -1,14 +1,21 @@
+# This file is executed inline by the by the Jython interpreter (i.e. it's not
+# "imported" in the usual Python sense).
+
+
 # There are some setter fields which the end user does not set:
 setter_blacklist = set(['setUniqueKey'])
 # TODO: Make some of fields optional (e.g. setxName)?
 
 
-def model(type, params):
-    # Degenerate case of compound model -> one list element.
-    return [bean('_'+type[0].upper()+type[1:]+'Model', params)]
+def bean(java_class, params, setter_blacklist=setter_blacklist):
+    """Instantiate a Javabean class from the global scope.
 
-
-def bean(java_class, params):
+    Each of the bean's setters is called with the corresponding value from
+    `params`, a dictionary. For instance, if the bean has a method called
+    `setLength`, this function will call it with the value `params['length']`.
+    If the key does not exist in `params`, this function will throw an
+    exception. Blacklisted setters will not be called.
+    """
     # The available model classes have been placed in our global scope by
     # Java. Choose one and instantiate it.
     bean_ = globals()[java_class]()
@@ -22,12 +29,31 @@ def bean(java_class, params):
     return bean_
 
 
+def model(type, params):
+    """Return a points model, wrapped in a list.
+
+    The idea behind the list wrapping is that, from the interpreter's point of
+    view, a "non-compound" scan is represented as a compound scan with a
+    single element; thus, a list with one element.
+
+    Then, to create a compound model we simply concatenate individual models.
+    """
+    # Degenerate case of compound model -> one list element.
+    return [bean('_'+type[0].upper()+type[1:]+'Model', params)]
+
+
 def compound(*points_models):
-    # This is just list concatenation.
+    """Concatenate points models to form a representation of a compound model.
+    """
     return reduce(lambda a, b: a + b, points_models)
 
 
 def scan(points_models, detector, exposure):
+    """Submit a scan request to the parent Java process.
+
+    `points_models` is a list. If its length is greater than one, this
+    represents a compound scan.
+    """
     # We could generate the full ScanModel here, but we'd probably end up
     # doing too much work in Python.
     _output.put(
@@ -63,6 +89,7 @@ def grid(axes=None, div=None, bbox=None, snake=False):
 def array(scannable, positions):
     # We have to manually call ArrayModel.setPositions,
     # as it takes a (Double... positions) argument.
+    # This is not ideal... TODO
     amodel = _ArrayModel()
     amodel.setName(scannable)
     amodel.setPositions(*positions)
