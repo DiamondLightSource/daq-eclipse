ab2276d135d9abc1779c2132a75b24ca224c43c2
Nic Bricknell <nic.bricknell@diamond.ac.uk>
2016-04-04 16:02:43 +0100

    Make the test.command Suite more complete
    http://jira.diamond.ac.uk/browse/DAQ-61

--- org.eclipse.scanning.test/src/org/eclipse/scanning/test/command/Suite.java
+++ org.eclipse.scanning.test/src/org/eclipse/scanning/test/command/Suite.java
@@ -6,5 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(org.junit.runners.Suite.class)
 @SuiteClasses({
 	CommandTest.class,
+	ModelStringifierTest.class,
+	RoundTripTest.class,
 })
 public class Suite { }
