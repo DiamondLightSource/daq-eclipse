eaba17852e09f977985ef6a535a73c920b053858
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-04-18 16:04:09 +0100

    Adds missing import.
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

--- org.eclipse.scanning.test/src/org/eclipse/scanning/test/event/queues/processors/Suite.java
+++ org.eclipse.scanning.test/src/org/eclipse/scanning/test/event/queues/processors/Suite.java
@@ -1,6 +1,7 @@
 package org.eclipse.scanning.test.event.queues.processors;
 
 import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(org.junit.runners.Suite.class)
 @SuiteClasses({
