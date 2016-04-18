30f8ea4b8989f4c7d82bb577f37aa7244d67fc5a
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-04-18 15:50:19 +0100

    Initial commit of test suite classes.
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

---
+++ org.eclipse.scanning.test/src/org/eclipse/scanning/test/event/queues/processors/Suite.java
@@ -0,0 +1,12 @@
+package org.eclipse.scanning.test.event.queues.processors;
+
+import org.junit.runner.RunWith;
+
+@RunWith(org.junit.runners.Suite.class)
+@SuiteClasses({
+	MoveAtomProcessorTest.class,
+	ScanAtomProcessorTest.class
+})
+public class Suite {
+
+}
