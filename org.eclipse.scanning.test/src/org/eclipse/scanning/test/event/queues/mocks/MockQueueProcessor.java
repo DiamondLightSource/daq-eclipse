6d760ff761b5bacc76374f2fafa3122ae277c8ce
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-05-16 10:52:38 +0100

    Complete's implementation of processor mock.
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

--- org.eclipse.scanning.test/src/org/eclipse/scanning/test/event/queues/mocks/MockQueueProcessor.java
+++ org.eclipse.scanning.test/src/org/eclipse/scanning/test/event/queues/mocks/MockQueueProcessor.java
@@ -3,26 +3,46 @@ package org.eclipse.scanning.test.event.queues.mocks;
 import java.util.concurrent.CountDownLatch;
 
 import org.eclipse.scanning.api.event.EventException;
-import org.eclipse.scanning.api.event.core.IConsumerProcess;
-import org.eclipse.scanning.api.event.core.IPublisher;
 import org.eclipse.scanning.api.event.queues.IQueueProcessor;
-import org.eclipse.scanning.api.event.queues.beans.Queueable;
 
 public class MockQueueProcessor implements IQueueProcessor {
-	
+
 	private CountDownLatch execLatch;
-	private long delay;
-	
+	private long delay = 500;
+
 	//Metrics for reporting interactions.
 	private long runTime = 0;
-	private boolean isExecuted;
-	private boolean isComplete;
-	private boolean isTerminated;
-	
+	private boolean executed;
+	private boolean complete;
+	private boolean terminated;
+
 	public MockQueueProcessor(CountDownLatch latch) {
 		execLatch = latch;
 	}
-	
+
+	@Override
+	public void execute() throws EventException, InterruptedException {
+		executed = true;
+		
+		long startTime = System.currentTimeMillis();
+		runTime = System.currentTimeMillis() - startTime;
+		while (runTime < delay) {
+			Thread.sleep(10);
+			runTime = System.currentTimeMillis() - startTime;
+			if (terminated) {
+				execLatch.countDown();
+				return;
+			}
+		}
+		complete = true;
+		execLatch.countDown();
+	}
+
+	@Override
+	public void terminate() throws EventException {
+		terminated = true;
+	}
+
 	public long getDelay() {
 		return delay;
 	}
@@ -36,15 +56,15 @@ public class MockQueueProcessor implements IQueueProcessor {
 	}
 
 	public boolean isExecuted() {
-		return isExecuted;
+		return executed;
 	}
 
 	public boolean isComplete() {
-		return isComplete;
+		return complete;
 	}
 
 	public boolean isTerminated() {
-		return isTerminated;
+		return terminated;
 	}
 
 }
