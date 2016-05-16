3e5443eaab01045c1d52421003262ffd12891057
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-05-16 10:52:24 +0100

    Updates IQueueProcessor for new model of process/processor.
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

--- org.eclipse.scanning.api/src/org/eclipse/scanning/api/event/queues/IQueueProcessor.java
+++ org.eclipse.scanning.api/src/org/eclipse/scanning/api/event/queues/IQueueProcessor.java
@@ -1,13 +1,11 @@
 package org.eclipse.scanning.api.event.queues;
 
 import org.eclipse.scanning.api.event.EventException;
-import org.eclipse.scanning.api.event.core.IConsumerProcess;
-import org.eclipse.scanning.api.event.core.IPublisher;
-import org.eclipse.scanning.api.event.queues.beans.Queueable;
 
 public interface IQueueProcessor {
 	
-	public <T extends Queueable> IConsumerProcess<T> makeProcess(T bean,
-			IPublisher<T> publisher, boolean blocking) throws EventException;
+	void execute() throws EventException, InterruptedException;
+	
+	void terminate() throws EventException;
 
 }
