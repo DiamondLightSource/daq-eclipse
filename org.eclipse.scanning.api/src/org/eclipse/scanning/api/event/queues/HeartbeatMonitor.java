23d1e54b27a7d4e11614786a552e14f61410454c
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-05-17 14:42:40 +0100

    Initial commit of HeartbeatMonitor and test.
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

---
+++ org.eclipse.scanning.api/src/org/eclipse/scanning/api/event/queues/HeartbeatMonitor.java
@@ -0,0 +1,28 @@
+package org.eclipse.scanning.api.event.queues;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.eclipse.scanning.api.event.alive.HeartbeatBean;
+
+public class HeartbeatMonitor {
+	
+	public HeartbeatMonitor(String queueID) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public HeartbeatMonitor(UUID consumerId) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public HeartbeatBean getLastHeartbeat() {
+		// TODO Auto-generated constructor stub
+		return null;
+	}
+	
+	public List<HeartbeatBean> getLatestHeartbeats() {
+		// TODO Auto-generated constructor stub
+		return null;
+	}
+
+}
---
+++ org.eclipse.scanning.test/src/org/eclipse/scanning/test/event/queues/api/HeartbeatMonitorTest.java
@@ -0,0 +1,62 @@
+package org.eclipse.scanning.test.event.queues.api;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.util.List;
+
+import org.eclipse.scanning.api.event.alive.HeartbeatBean;
+import org.eclipse.scanning.api.event.core.IConsumer;
+import org.eclipse.scanning.api.event.queues.HeartbeatMonitor;
+import org.eclipse.scanning.test.event.queues.mocks.DummyBean;
+import org.eclipse.scanning.test.event.queues.util.EventServiceActorMaker;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HeartbeatMonitorTest {
+	
+	private IConsumer<DummyBean> cons;
+	private HeartbeatMonitor hbM;
+	
+	@Before
+	public void setUp() {
+		cons = EventServiceActorMaker.makeConsumer(new DummyBean(), true);
+	}
+	
+	@Test
+	public void testLatestHeartbeats() throws Exception {
+		hbM = new HeartbeatMonitor(cons.getConsumerId());
+		cons.start();
+		Thread.sleep(4300);
+		
+		List<HeartbeatBean> heartbeats = hbM.getLatestHeartbeats();
+		assertEquals("Unexpected number of heartbeats", 2, heartbeats.size());
+	}
+	
+	@Test
+	public void testLastHeartbeat() throws Exception {
+		hbM = new HeartbeatMonitor(cons.getConsumerId());
+		cons.start();
+		Thread.sleep(2300);
+		
+		final HeartbeatBean first = hbM.getLastHeartbeat();
+		Thread.sleep(2300);
+		final HeartbeatBean second = hbM.getLastHeartbeat();
+		cons.start();
+		List<HeartbeatBean> heartbeats = hbM.getLatestHeartbeats();
+		
+		assertFalse("First & second heartbeats are the same", first.equals(second));
+		assertEquals("Last heartbeat in latest and lastBeat differ", second, heartbeats.get(heartbeats.size()-1));
+	}
+	
+//	@Test
+//	public void testChangingQueueID() {
+//		//This needs a mock queue object
+//	}
+//	
+//	@Test
+//	public void testFixedQueueMonitor() {
+//		
+//	}
+
+}
