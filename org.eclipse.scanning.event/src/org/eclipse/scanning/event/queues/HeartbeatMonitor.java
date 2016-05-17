64582828de831680c29b67e01c59dc43ed18abb4
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-05-17 15:30:13 +0100

    Adds missing method stubs.
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

--- org.eclipse.scanning.event/src/org/eclipse/scanning/event/queues/HeartbeatMonitor.java
+++ org.eclipse.scanning.event/src/org/eclipse/scanning/event/queues/HeartbeatMonitor.java
@@ -4,25 +4,83 @@ import java.util.List;
 import java.util.UUID;
 
 import org.eclipse.scanning.api.event.alive.HeartbeatBean;
+import org.eclipse.scanning.api.event.queues.IHeartbeatMonitor;
+import org.eclipse.scanning.api.event.queues.IQueueService;
 
-public class HeartbeatMonitor {
+public class HeartbeatMonitor implements IHeartbeatMonitor {
+	
+	public HeartbeatMonitor(UUID consumerID) {
+		
+	}
 	
 	public HeartbeatMonitor(String queueID) {
-		// TODO Auto-generated constructor stub
+		
 	}
 	
-	public HeartbeatMonitor(UUID consumerId) {
-		// TODO Auto-generated constructor stub
+	public HeartbeatMonitor (String queueID, IQueueService queueService) {
+		
 	}
 
+	@Override
 	public HeartbeatBean getLastHeartbeat() {
-		// TODO Auto-generated constructor stub
+		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<HeartbeatBean> getLatestHeartbeats() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHeartbeatTopic() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setHeartbeatTopic(String topicName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public UUID getConsumerID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setConsumerID(UUID consumerID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getQueueID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setQueueID(String queueID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getRecorderSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setRecorderSize(int beats) {
+		// TODO Auto-generated method stub
+		
 	}
 	
-	public List<HeartbeatBean> getLatestHeartbeats() {
-		// TODO Auto-generated constructor stub
-		return null;
-	}
+
 
 }
