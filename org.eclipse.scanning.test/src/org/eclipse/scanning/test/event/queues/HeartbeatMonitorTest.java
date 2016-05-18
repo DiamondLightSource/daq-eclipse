5bdad458850b9de19d1d83a2b0f3fc96102cd95d
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-05-18 14:30:52 +0100

    Moves test to correct location.
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

--- org.eclipse.scanning.test/src/org/eclipse/scanning/test/event/queues/HeartbeatMonitorTest.java
+++ org.eclipse.scanning.test/src/org/eclipse/scanning/test/event/queues/HeartbeatMonitorTest.java
@@ -1,4 +1,4 @@
-package org.eclipse.scanning.test.event.queues.api;
+package org.eclipse.scanning.test.event.queues;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
