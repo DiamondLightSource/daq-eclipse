61ececd6d60266cf98f1a2a97f7e30298171e433
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-06-01 00:36:09 +0100

    Makes test general for all three Dummy bean types. Adds exception checking test.
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

--- org.eclipse.scanning.test/src/org/eclipse/scanning/test/event/queues/dummy/DummyProcessingTest.java
+++ org.eclipse.scanning.test/src/org/eclipse/scanning/test/event/queues/dummy/DummyProcessingTest.java
@@ -1,11 +1,15 @@
 package org.eclipse.scanning.test.event.queues.dummy;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
 
 import java.util.concurrent.CountDownLatch;
 
+import org.eclipse.scanning.api.event.EventException;
 import org.eclipse.scanning.api.event.core.IPublisher;
 import org.eclipse.scanning.api.event.queues.IQueueProcess;
+import org.eclipse.scanning.api.event.queues.IQueueProcessor;
 import org.eclipse.scanning.api.event.queues.beans.Queueable;
 import org.eclipse.scanning.api.event.status.Status;
 import org.eclipse.scanning.event.queues.QueueProcess;
@@ -18,6 +22,10 @@ public class DummyProcessingTest {
 	private DummyAtom dAt = new DummyAtom("Clotho", 100);
 	private DummyBean dBe = new DummyBean("Lachesis", 200);
 	private DummyHasQueue dHQ = new DummyHasQueue("Atropos", 300);
+	
+	private DummyAtomProcessor dAtProcr;
+	private DummyBeanProcessor dBeProcr;
+	private DummyHasQueueProcessor dHQProcr;
 	
 	private CountDownLatch execLatch;
 	
@@ -33,17 +41,75 @@ public class DummyProcessingTest {
 	@Test
 	public void testDummyAtomRunning() throws Exception {
 		dAt.setLatch(execLatch);
-		DummyAtomProcessor dAtProc= new DummyAtomProcessor(dAt);
-		qProc = new QueueProcess<Queueable>(dAt, pub, true, dAtProc);
+		dAtProcr= new DummyAtomProcessor();
 		
-		assertEquals("Wrong initial status", Status.NONE, dAt.getStatus());
-		assertEquals("Should not be non-zero percent complete", 0d, dAt.getPercentComplete(), 0);
+		runTest(dAtProcr, dAt);
+	}
+	
+	@Test
+	public void testDummyBeanRunning() throws Exception {
+		dBe.setLatch(execLatch);
+		dBeProcr = new DummyBeanProcessor();
+		
+		runTest(dBeProcr, dBe);
+	}
+	
+	@Test
+	public void testDummyHasQueueRunning() throws Exception {
+		dHQ.setLatch(execLatch);
+		dHQProcr = new DummyHasQueueProcessor();
+		
+		runTest(dHQProcr, dHQ);
+	}
+	
+	@Test
+	public void testErrorsOnChangeProcess() throws Exception {
+		DummyAtomProcessor dAtProcr = new DummyAtomProcessor();
+		qProc = new QueueProcess<Queueable>(dAt, pub, true);
+		
+		//Mimic processor execution
+		dAtProcr.setExecuted();
+		try {
+			dAtProcr.setProcessBean(dAt);
+			fail("Should not be able to set bean after execution start");
+		} catch (EventException eEx) {
+			//Expected
+		}
+		try {
+			dAtProcr.setQueueProcess(qProc);
+			fail("Should not be able to set process after execution start");
+		} catch (EventException eEx) {
+			//Expected
+		}
+		
+		//Try for real
+		dAtProcr = new DummyAtomProcessor();
+		qProc = new QueueProcess<Queueable>(dAt, pub, true);
+		assertFalse("Executed should initially be false", dAtProcr.isExecuted());
+		
+		//Configure the processor & process
+		dAtProcr.setProcessBean(dAt);
+		dAtProcr.setQueueProcess(qProc);
+		qProc.setProcessor(dAtProcr);
+	}
+	
+	private void runTest(IQueueProcessor<? extends Queueable> qProcr, Queueable beAt) throws Exception {
+		qProc = new QueueProcess<Queueable>(beAt, pub, true);
+		
+		
+		//Configure the processor & process
+		qProcr.setProcessBean(beAt);
+		qProcr.setQueueProcess(qProc);
+		qProc.setProcessor(qProcr);
+		
+		assertEquals("Wrong initial status", Status.NONE, beAt.getStatus());
+		assertEquals("Should not be non-zero percent complete", 0d, beAt.getPercentComplete(), 0);
 		
 		qProc.execute();
 		execLatch.await();
 		
-		assertEquals("Wrong final status", Status.COMPLETE, dAt.getStatus());
-		assertEquals("Should be 100 percent complete at end", 100d, dAt.getPercentComplete(), 0);
+		assertEquals("Wrong final status", Status.COMPLETE, beAt.getStatus());
+		assertEquals("Should be 100 percent complete at end", 100d, beAt.getPercentComplete(), 0);
 		
 	}
 
