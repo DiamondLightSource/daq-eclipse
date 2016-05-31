63e007d00944d85a0f1db175619d0f059cef2429
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-05-31 11:05:24 +0100

    Adds bean() default method to IQueueProcessor.
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

--- org.eclipse.scanning.api/src/org/eclipse/scanning/api/event/queues/IQueueProcessor.java
+++ org.eclipse.scanning.api/src/org/eclipse/scanning/api/event/queues/IQueueProcessor.java
@@ -1,9 +1,10 @@
 package org.eclipse.scanning.api.event.queues;
 
 import org.eclipse.scanning.api.event.EventException;
+import org.eclipse.scanning.api.event.core.IConsumerProcess;
 import org.eclipse.scanning.api.event.queues.beans.Queueable;
 
-public interface IQueueProcessor<T extends Queueable> {
+public interface IQueueProcessor<Q extends Queueable, T extends Q> {
 	
 	public void execute() throws EventException, InterruptedException;
 	
@@ -16,5 +17,20 @@ public interface IQueueProcessor<T extends Queueable> {
 	 * @return String class name of processable atom/bean type.
 	 */
 	public String getAtomBeanType();
+	
+	public IConsumerProcess<Q> getProcess();
+	
+	public Class<T> getBeanClass();
+	
+	@SuppressWarnings("unchecked")
+	public default T bean() throws EventException{
+		Q bean = getProcess().getBean();
+		
+		if (bean.getClass() == getBeanClass()) {
+			return (T) bean;
+		} else {
+			throw new EventException("Bean class "+getBeanClass()+" not supported. Expecting "+getBeanClass()); 
+		}
+	}
 
 }
