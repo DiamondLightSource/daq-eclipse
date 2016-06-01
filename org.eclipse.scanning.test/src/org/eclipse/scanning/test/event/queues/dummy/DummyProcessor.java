4f5a5ceab54aed2bc2e789e3409893fc9e2b836b
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-06-01 11:39:48 +0100

    Replaces execute method with contents of run method. Threading now handled external to the processor (in the test in this case).
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

--- org.eclipse.scanning.test/src/org/eclipse/scanning/test/event/queues/dummy/DummyProcessor.java
+++ org.eclipse.scanning.test/src/org/eclipse/scanning/test/event/queues/dummy/DummyProcessor.java
@@ -1,7 +1,5 @@
 package org.eclipse.scanning.test.event.queues.dummy;
 
-import java.util.concurrent.CountDownLatch;
-
 import org.eclipse.scanning.api.event.EventException;
 import org.eclipse.scanning.api.event.queues.IQueueProcess;
 import org.eclipse.scanning.api.event.queues.IQueueProcessor;
@@ -15,29 +13,33 @@ public abstract class DummyProcessor <P extends Queueable> implements IQueueProc
 	private static final Logger logger = LoggerFactory.getLogger(DummyProcessor.class);
 	
 	private IQueueProcess<? extends Queueable> process;
-	private boolean terminated, executed = false;
+	private boolean terminated = false, executed = false;
 	
 	protected P dummy;
-	protected CountDownLatch execLatch;
 	
 	@Override
 	public void execute() throws EventException {
-		if (!(dummy.equals(process.getBean()))) throw new EventException("Beans on QueueProcess and QueueProcessor differ");
 		setExecuted();
-		execLatch = getLatch();
+		if (!(dummy.equals(process.getBean()))) throw new EventException("Beans on QueueProcess and QueueProcessor differ");
 		
-		final Thread thread = new Thread(new Runnable() {
-			public void run() {
-				try {
-					DummyProcessor.this.run();
-				} catch (EventException ne) {
-					ne.printStackTrace(); // Only a test process!
-				}
+		process.broadcast(Status.RUNNING, 0d);
+
+		for (int i = 0; i < 100; i++) {
+			if (isTerminated()) {
+				process.broadcast(Status.TERMINATED);
+				return;
 			}
-		});
-		thread.setDaemon(true);
-		thread.setPriority(Thread.MAX_PRIORITY);
-		thread.start();
+
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				logger.error("Dummy process sleeping failed", e);
+				throw new EventException(e);
+			}
+			System.out.println("DummyProcessor ("+dummy.getClass().getSimpleName()+" - "+dummy.getName()+"): "+dummy.getPercentComplete());
+			process.broadcast(new Double(i));
+		}
+		process.broadcast(Status.COMPLETE, 100d, "Dummy process complete (no software run)");
 	}
 	
 	@Override
@@ -72,38 +74,12 @@ public abstract class DummyProcessor <P extends Queueable> implements IQueueProc
 		this.process = process;
 	}
 	
-	protected abstract CountDownLatch getLatch();
-	
-	private void run() throws EventException {
-		process.broadcast(Status.RUNNING, 0d);
-
-		terminated = false;
-
-		for (int i = 0; i < 100; i++) {
-
-			if (isTerminated()) {
-				process.broadcast(Status.TERMINATED);
-				return;
-			}
-
-			try {
-				Thread.sleep(50);
-			} catch (InterruptedException e) {
-				logger.error("Dummy process sleeping failed", e);
-			}
-			System.out.println("DummyProcessor ("+dummy.getClass().getSimpleName()+" - "+dummy.getName()+"): "+dummy.getPercentComplete());
-			process.broadcast(new Double(i));
-		}
-		process.broadcast(Status.COMPLETE, 100d, "Dummy process complete (no software run)");
-		if (execLatch != null) execLatch.countDown();
-	}
-	
 	public boolean isTerminated() {
 		return terminated;
 	}
 
-	public void setTerminated(boolean terminated) {
-		this.terminated = terminated;
+	public void setTerminated() {
+		terminated = true;
 	}
 	
 	@Override
