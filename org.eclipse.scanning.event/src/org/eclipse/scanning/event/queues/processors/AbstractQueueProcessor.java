78348a3ce30366d714387d271cacef889fefdcb5
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-06-01 19:12:06 +0100

    Adds logging.
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

--- org.eclipse.scanning.event/src/org/eclipse/scanning/event/queues/processors/AbstractQueueProcessor.java
+++ org.eclipse.scanning.event/src/org/eclipse/scanning/event/queues/processors/AbstractQueueProcessor.java
@@ -4,8 +4,12 @@ import org.eclipse.scanning.api.event.EventException;
 import org.eclipse.scanning.api.event.queues.IQueueProcess;
 import org.eclipse.scanning.api.event.queues.IQueueProcessor;
 import org.eclipse.scanning.api.event.queues.beans.Queueable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class AbstractQueueProcessor <P extends Queueable> implements IQueueProcessor<P> {
+	
+	private static Logger logger = LoggerFactory.getLogger(AbstractQueueProcessor.class);
 	
 	private boolean terminated = false, executed = false;
 	
@@ -15,17 +19,24 @@ public abstract class AbstractQueueProcessor <P extends Queueable> implements IQ
 	@SuppressWarnings("unchecked")
 	@Override
 	public <T extends Queueable> void setProcessBean(T bean) throws EventException {
-		if (isExecuted()) throw new EventException("Cannot change queueProcess after execution started.");
+		if (isExecuted()) {
+			logger.error("Cannot change bean to be processed after execution has started.");
+			throw new EventException("Cannot change bean to be processed after execution has started");
+		}
 		if (bean.getClass().equals(getBeanClass())) {
 			this.bean = (P)bean;
 		} else {
+			logger.error("Cannot set bean: Bean type "+bean.getClass().getSimpleName()+" not supported by "+getClass().getSimpleName()+".");
 			throw new EventException("Unsupported bean type");
 		}
 	}
 
 	@Override
 	public void setQueueProcess(IQueueProcess<? extends Queueable> process) throws EventException {
-		if (isExecuted()) throw new EventException("Cannot change queueProcess after execution started.");
+		if (isExecuted()) {
+			logger.error("Cannot change IQueueProcess after execution has started.");
+			throw new EventException("Cannot change IQueueProcess after execution has started");
+		}
 		this.process = process;
 	}
 
