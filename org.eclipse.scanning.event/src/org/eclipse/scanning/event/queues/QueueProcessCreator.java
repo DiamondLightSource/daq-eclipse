2780fb98f7b409652adfc08a02164bf62c850e8b
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-06-08 14:26:51 +0100

    Refactors process creator for new architecture.
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

--- org.eclipse.scanning.event/src/org/eclipse/scanning/event/queues/QueueProcessCreator.java
+++ org.eclipse.scanning.event/src/org/eclipse/scanning/event/queues/QueueProcessCreator.java
@@ -1,24 +1,13 @@
 package org.eclipse.scanning.event.queues;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.eclipse.scanning.api.event.EventException;
 import org.eclipse.scanning.api.event.core.IConsumerProcess;
 import org.eclipse.scanning.api.event.core.IProcessCreator;
 import org.eclipse.scanning.api.event.core.IPublisher;
+import org.eclipse.scanning.api.event.queues.IQueueProcess;
 import org.eclipse.scanning.api.event.queues.IQueueProcessor;
 import org.eclipse.scanning.api.event.queues.beans.QueueAtom;
 import org.eclipse.scanning.api.event.queues.beans.Queueable;
-import org.eclipse.scanning.event.queues.beans.MonitorAtom;
-import org.eclipse.scanning.event.queues.beans.MoveAtom;
-import org.eclipse.scanning.event.queues.beans.ScanAtom;
-import org.eclipse.scanning.event.queues.beans.SubTaskBean;
-import org.eclipse.scanning.event.queues.beans.TaskBean;
-import org.eclipse.scanning.event.queues.processors.AtomQueueProcessor;
-import org.eclipse.scanning.event.queues.processors.MonitorAtomProcessor;
-import org.eclipse.scanning.event.queues.processors.MoveAtomProcessor;
-import org.eclipse.scanning.event.queues.processors.ScanAtomProcessor;
 
 /**
  * QueueProcessCreator creates the class which processes a given atom/bean. 
@@ -31,32 +20,28 @@ import org.eclipse.scanning.event.queues.processors.ScanAtomProcessor;
  */
 public class QueueProcessCreator<T extends Queueable> implements IProcessCreator<T> {
 
-	private Map<String, IQueueProcessor> processMap;
 	private boolean blocking;
+	private IQueueProcess<T> queueProcess;
+	private IQueueProcessor<? extends Queueable> processor;
 
-	public QueueProcessCreator(boolean blocking) {
+	public QueueProcessCreator(boolean blocking) { //TODO Do we need this?
 		this.blocking = blocking;
 	}
 
 	@Override
 	public IConsumerProcess<T> createProcess(T atomBean,
 			IPublisher<T> statusNotifier) throws EventException {
-
-		//Create a map of bean type to processor
-		processMap = new HashMap<String, IQueueProcessor>();
-		processMap.put(MonitorAtom.class.getSimpleName(), new MonitorAtomProcessor());
-		processMap.put(MoveAtom.class.getSimpleName(), new MoveAtomProcessor());
-		processMap.put(ScanAtom.class.getSimpleName(), new ScanAtomProcessor());
-		processMap.put(SubTaskBean.class.getSimpleName(), new AtomQueueProcessor());
-		processMap.put(TaskBean.class.getSimpleName(), new AtomQueueProcessor());
-
-		//Determine the type of bean and return the appropriate processor
-		String className = atomBean.getClass().getSimpleName();
-		if (processMap.containsKey(className)) {
-			if(processMap.get(className) == null) throw new EventException("No processor registered for bean");
-			return processMap.get(className).makeProcess(atomBean, statusNotifier, blocking);
-		}
-		throw new EventException("Bean type '"+className+"' not registered in queue processMap.");
+		//Create the process
+		queueProcess = new QueueProcess<>(atomBean, statusNotifier, blocking);
+		
+		//Create & configure the processor
+		processor = QueueProcessorFactory.getProcessor(atomBean.getClass().getName());
+		processor.setProcessBean(atomBean);
+		processor.setQueueBroadcaster(queueProcess);
+		
+		//Finish process configuration & return
+		queueProcess.setProcessor(processor);
+		return queueProcess;
 	}
 
 }
