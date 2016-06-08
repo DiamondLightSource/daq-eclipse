c5b4998bcde5b3a911e01fc665f350cd1a77a908
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-06-08 15:30:10 +0100

    Adds merge calls to queue beans.
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

--- org.eclipse.scanning.test/src/org/eclipse/scanning/test/event/queues/dummy/DummyHasQueue.java
+++ org.eclipse.scanning.test/src/org/eclipse/scanning/test/event/queues/dummy/DummyHasQueue.java
@@ -32,5 +32,37 @@ public class DummyHasQueue extends Queueable {
 	public void setQueueMessage(String queueMessage) {
 		this.queueMessage = queueMessage;
 	}
+	
+	public void merge(DummyHasQueue with) {
+		super.merge(with);
+		this.queueMessage = with.queueMessage;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((queueMessage == null) ? 0 : queueMessage.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DummyHasQueue other = (DummyHasQueue) obj;
+		if (queueMessage == null) {
+			if (other.queueMessage != null)
+				return false;
+		} else if (!queueMessage.equals(other.queueMessage))
+			return false;
+		return true;
+	}
+	
+	
 
 }
