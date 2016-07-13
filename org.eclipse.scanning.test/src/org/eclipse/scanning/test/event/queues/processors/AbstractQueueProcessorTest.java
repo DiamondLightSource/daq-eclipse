e7a7d42f7ca961e638b1724e1c3b0eeee1e5a51a
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-07-13 13:59:45 +0100

    Removes println and removes section of commented old code.
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

--- org.eclipse.scanning.test/src/org/eclipse/scanning/test/event/queues/processors/AbstractQueueProcessorTest.java
+++ org.eclipse.scanning.test/src/org/eclipse/scanning/test/event/queues/processors/AbstractQueueProcessorTest.java
@@ -353,7 +353,6 @@ public abstract class AbstractQueueProcessorTest {
 		
 		while (runTime <= timeout) {
 			if ((lastBean != null) && (lastBean.getUniqueId().equals(bean.getUniqueId()))) {
-				System.out.println(lastBean.getStatus());
 				if ((lastBean.getStatus().equals(state)) || (lastBean.getStatus().isFinal() && isFinal)) {
 					return;
 				}
@@ -482,93 +481,4 @@ public abstract class AbstractQueueProcessorTest {
 		return broadcastBeans;
 	}
 
-//	protected void checkBeanFinalStatus(Status expected, boolean testLastButTwoPerc) throws Exception {
-//		if (thrownException != null) { //I think this is a bit ugly, but it's only a test.
-//			throw thrownException;
-//		}
-//		
-//		DummyHasQueue firstBean, lastButTwoBean, lastBean;
-//		
-//		List<Queueable> broadcastBeans = ((MockPublisher<QueueAtom>)statPub).getBroadcastBeans();
-//		firstBean =  broadcastBeans.get(0);
-//		lastButTwoBean = broadcastBeans.get(broadcastBeans.size()-3);
-//		lastBean = broadcastBeans.get(broadcastBeans.size()-1);
-//		
-//		assertEquals("First bean has wrong status", Status.RUNNING, firstBean.getStatus());
-//		
-//		assertEquals("Last bean has wrong status", expected, lastBean.getStatus());
-//		if (testLastButTwoPerc) {
-//			System.out.println("Last-but-two % complete: "+lastButTwoBean.getPercentComplete());
-//			if (expected.equals(Status.TERMINATED)) {
-//				assertEquals("Second to last bean has wrong status", Status.REQUEST_TERMINATE, lastButTwoBean.getStatus());
-//			} else if (expected.equals(Status.COMPLETE)){
-//				assertEquals("Second to last bean has wrong status", Status.RUNNING, lastButTwoBean.getStatus());
-//				assertEquals("Second to last bean not almost complete", 99d, lastButTwoBean.getPercentComplete(), 5d);
-//			}	
-//		}
-//		if (expected.equals(Status.COMPLETE)) {
-//			assertEquals("Atom not 100% complete after execution", 100, lastBean.getPercentComplete(), 0);}
-//		else {
-//			assertThat("The percent complete is 100!", lastBean.getPercentComplete(), is(not(100)));
-//		}
-//	}
-//	
-//	protected void checkBeanFinalStatus(Status expected) throws Exception {
-//		checkBeanFinalStatus(expected, false);
-//	}
-//	
-//	
-//	protected void checkBeanStatuses(Status[] repStat, Double[] repPerc) throws Exception {
-//		if (thrownException != null) { //I think this is a bit ugly, but it's only a test.
-//			throw thrownException;
-//		}
-//		
-//		if(repStat.length != repPerc.length) throw new Exception("Different numbers of statuses & percentages given!");
-//		
-//		List<DummyHasQueue> broadcastBeans = ((MockPublisher<QueueAtom>)statPub).getBroadcastBeans();
-//		DummyHasQueue bean;
-//		for(int i = 0; i < repStat.length; i++) {
-//			bean = broadcastBeans.get(i);
-//			assertEquals("Unexpected status for bean "+(i+1), repStat[i], bean.getStatus());
-//			assertEquals("Unexpected percent complete for bean "+(i+1), (double)repPerc[i],bean.getPercentComplete(), 0.05);
-//		}
-//	}
-//
-//	protected void pauseForMockFinalStatus(long timeOut) throws Exception {
-//		boolean notFinal = true;
-//		DummyHasQueue lastBean = getLastBean(timeOut);
-//		long startTime = System.currentTimeMillis();
-//		
-//		while (notFinal) {
-//			if (lastBean.getStatus().isFinal()) return;
-//			Thread.sleep(100);
-//			
-//			if (startTime-System.currentTimeMillis() >= timeOut) fail("Final state not found before timeout");
-//			lastBean = getLastBean(timeOut);
-//		}
-//	}
-//	
-//	protected void pauseForMockStatus(Status expected, long timeOut) throws Exception {
-//		DummyHasQueue lastBean = getLastBean(timeOut);
-//		long startTime = System.currentTimeMillis();
-//		
-//		while (lastBean.getStatus() != expected) {
-//			Thread.sleep(100);
-//			
-//			if (startTime-System.currentTimeMillis() >= timeOut) fail(expected+" not found before timeout");
-//			lastBean = getLastBean(timeOut);
-//		}
-//	}
-//	
-//	private DummyHasQueue getLastBean(long timeOut) throws Exception {
-//		List<DummyHasQueue> broadcastBeans = ((MockPublisher<QueueAtom>)statPub).getBroadcastBeans();
-//		long startTime = System.currentTimeMillis();
-//		while (broadcastBeans.size() == 0) {
-//			Thread.sleep(100);
-//			if (startTime-System.currentTimeMillis() >= timeOut) fail("No beans broadcast before timeout");
-//			broadcastBeans = ((MockPublisher<QueueAtom>)statPub).getBroadcastBeans();
-//		}
-//		
-//		return broadcastBeans.get(broadcastBeans.size()-1);
-//	}
 }
