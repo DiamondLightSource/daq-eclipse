0ddf1343a48de08519897388c32a787dbc38acb4
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-08-05 11:48:48 +0100

    Adds terminateQueueProcess method and generic method for getting queue status topic.
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

--- org.eclipse.scanning.event/src/org/eclipse/scanning/event/queues/AtomQueueServiceUtils.java
+++ org.eclipse.scanning.event/src/org/eclipse/scanning/event/queues/AtomQueueServiceUtils.java
@@ -5,19 +5,44 @@ import java.util.EventListener;
 
 import org.eclipse.scanning.api.event.EventException;
 import org.eclipse.scanning.api.event.IEventService;
+import org.eclipse.scanning.api.event.core.IPublisher;
 import org.eclipse.scanning.api.event.core.ISubscriber;
 import org.eclipse.scanning.api.event.queues.IQueueService;
+import org.eclipse.scanning.api.event.queues.beans.IQueueable;
+import org.eclipse.scanning.api.event.status.Status;
 
 public class AtomQueueServiceUtils {
+	
 	
 	private static IEventService eventService = QueueServicesHolder.getEventService();
 	private static IQueueService queueService = QueueServicesHolder.getQueueService();
 	
-	public static <T extends EventListener> ISubscriber<T> createActiveQueueSubscriber(String queueID) throws EventException {
+	public static <T extends EventListener> ISubscriber<T> createQueueSubscriber(String queueID) throws EventException {
 		URI uri = queueService.getURI();
-		String topicName = queueService.getActiveQueue(queueID).getStatusTopicName();
+		String topicName = getQueueStatusTopicName(queueID);
 		
 		return eventService.createSubscriber(uri, topicName);
+	}
+	
+	public static void terminateQueueProcess(String queueID, IQueueable atomBean) throws EventException {
+		//Get configuration for 
+		URI uri = queueService.getURI();
+		String topicName = getQueueStatusTopicName(queueID);
+		
+		atomBean.setPreviousStatus(atomBean.getPreviousStatus());
+		atomBean.setStatus(Status.REQUEST_TERMINATE);
+		
+		IPublisher<IQueueable> terminator = eventService.createPublisher(uri, topicName);
+		terminator.broadcast(atomBean);
+		terminator.disconnect();
+	}
+	
+	private static String getQueueStatusTopicName(String queueID) {
+		if (queueID == queueService.getJobQueueID()) {
+			return queueService.getJobQueue().getStatusTopicName();
+		} else {
+			return queueService.getActiveQueue(queueID).getStatusTopicName();
+		}
 	}
 
 }
