5bd620efaa8f40d2afa06b758bcd94d675151dbc
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-08-05 11:50:45 +0100

    Adds termination handling to SubTaskAtomProcessor and AtomQueueProcessor.

--- org.eclipse.scanning.event/src/org/eclipse/scanning/event/queues/processors/SubTaskAtomProcessor.java
+++ org.eclipse.scanning.event/src/org/eclipse/scanning/event/queues/processors/SubTaskAtomProcessor.java
@@ -3,6 +3,7 @@ package org.eclipse.scanning.event.queues.processors;
 import org.eclipse.scanning.api.event.EventException;
 import org.eclipse.scanning.api.event.queues.beans.QueueAtom;
 import org.eclipse.scanning.api.event.status.Status;
+import org.eclipse.scanning.event.queues.AtomQueueServiceUtils;
 import org.eclipse.scanning.event.queues.beans.SubTaskAtom;
 
 public class SubTaskAtomProcessor extends AbstractQueueProcessor<SubTaskAtom> {
@@ -15,14 +16,20 @@ public class SubTaskAtomProcessor extends AbstractQueueProcessor<SubTaskAtom> {
 
 	@Override
 	public void execute() throws EventException, InterruptedException {
+		setExecuted();
 		//Do most of the work of processing in the atomQueueProcessor...
 		atomQueueProcessor.run();
 		
 		//...do the post-match analysis in here!
-//		if (queueBean.getPercentComplete() >= 99.5) {
+		if (isTerminated()) {
+			broadcaster.broadcast("Active-queue aborted before completion (requested)");
+			AtomQueueServiceUtils.terminateQueueProcess(atomQueueProcessor.getActiveQueueName(), queueBean);
+		}
+		
+		if (queueBean.getPercentComplete() >= 99.5) {
 			//Completed successfully
 			broadcaster.broadcast(Status.COMPLETE, 100d, "Scan completed.");
-//		}
+		}
 
 	}
 
@@ -40,7 +47,8 @@ public class SubTaskAtomProcessor extends AbstractQueueProcessor<SubTaskAtom> {
 
 	@Override
 	public void terminate() throws EventException {
-		// TODO Auto-generated method stub
+		setTerminated();
+		processorLatch.countDown();
 
 	}
 
