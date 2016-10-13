8f7eed45e7d588be49a50be643aff388846332c1
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-10-13 22:31:52 +0100

    Brings Queue in-line with new API for IQueue.
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

--- org.eclipse.scanning.test/src/org/eclipse/scanning/test/event/queues/mocks/MockQueue.java
+++ org.eclipse.scanning.test/src/org/eclipse/scanning/test/event/queues/mocks/MockQueue.java
@@ -1,15 +1,11 @@
 package org.eclipse.scanning.test.event.queues.mocks;
 
-import java.util.List;
-import java.util.Map;
+import java.net.URI;
 import java.util.UUID;
 
 import org.eclipse.scanning.api.event.EventException;
 import org.eclipse.scanning.api.event.IEventService;
-import org.eclipse.scanning.api.event.alive.HeartbeatBean;
 import org.eclipse.scanning.api.event.core.IConsumer;
-import org.eclipse.scanning.api.event.core.IProcessCreator;
-import org.eclipse.scanning.api.event.queues.IHeartbeatMonitor;
 import org.eclipse.scanning.api.event.queues.IQueue;
 import org.eclipse.scanning.api.event.queues.QueueStatus;
 import org.eclipse.scanning.api.event.queues.beans.Queueable;
@@ -30,13 +26,13 @@ public class MockQueue<T extends Queueable> implements IQueue<T> {
 	}
 
 	@Override
-	public QueueStatus getQueueStatus() {
+	public QueueStatus getStatus() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void setQueueStatus(QueueStatus status) {
+	public void setStatus(QueueStatus status) {
 		// TODO Auto-generated method stub
 		
 	}
@@ -52,30 +48,6 @@ public class MockQueue<T extends Queueable> implements IQueue<T> {
 	}
 
 	@Override
-	public IProcessCreator<T> getProcessRunner() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setProcessRunner(IProcessCreator<T> processor) throws EventException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public List<HeartbeatBean> getLatestHeartbeats() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public HeartbeatBean getLastHeartbeat() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public boolean clearQueues() throws EventException {
 		// TODO Auto-generated method stub
 		return false;
@@ -88,25 +60,7 @@ public class MockQueue<T extends Queueable> implements IQueue<T> {
 	}
 
 	@Override
-	public boolean hasSubmittedJobsPending() throws EventException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Map<String, String> getQueueNames() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public String getSubmissionQueueName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getStatusQueueName() {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -130,13 +84,31 @@ public class MockQueue<T extends Queueable> implements IQueue<T> {
 	}
 
 	@Override
-	public IHeartbeatMonitor getHeartbeatMonitor() {
+	public void start() throws EventException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() throws EventException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getStatusSetName() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public String getCommandQueueName() {
+	public String getCommandSetName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public URI getURI() {
 		// TODO Auto-generated method stub
 		return null;
 	}
