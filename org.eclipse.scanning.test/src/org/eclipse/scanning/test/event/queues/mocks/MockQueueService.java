233dd638183bfa354af9ad3bcf59fa395d8116e2
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-10-13 22:36:46 +0100

    Removes unnecessary import.
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

--- org.eclipse.scanning.test/src/org/eclipse/scanning/test/event/queues/mocks/MockQueueService.java
+++ org.eclipse.scanning.test/src/org/eclipse/scanning/test/event/queues/mocks/MockQueueService.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import java.util.Set;
 
 import org.eclipse.scanning.api.event.EventException;
