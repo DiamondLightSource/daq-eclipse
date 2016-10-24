b2b31c1a58716ff950561d4cf974a1f03d0c8f59
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-10-24 11:15:04 +0100

    Initial commit of classes for remote service.
    New packages o.e.s.event.queues.remote and o.e.s.api.queues.remote containing initial classes referenced in test implementation. Adds export of API class.
    
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

---
+++ org.eclipse.scanning.api/src/org/eclipse/scanning/api/event/queues/remote/QueueRequest.java
@@ -0,0 +1,30 @@
+package org.eclipse.scanning.api.event.queues.remote;
+
+import org.eclipse.scanning.api.event.IdBean;
+import org.eclipse.scanning.api.event.status.Status;
+
+public class QueueRequest extends IdBean {
+	
+	public void setBeanID(String beanID) {
+		//TODO
+	}
+	
+	public void setQueueID(String queueID) {
+		//TODO
+	}
+	
+	public void setType(QueueRequestType type) {
+		//TODO
+	}
+	
+	public Status getBeanStatus() {
+		//TODO
+		return null;
+	}
+	
+	public String getJobQueueID() {
+		//TODO
+		return null;
+	}
+
+}
---
+++ org.eclipse.scanning.api/src/org/eclipse/scanning/api/event/queues/remote/QueueRequestType.java
@@ -0,0 +1,5 @@
+package org.eclipse.scanning.api.event.queues.remote;
+
+public enum QueueRequestType {
+	BEAN_STATUS, JOB_QUEUE_ID;
+}
