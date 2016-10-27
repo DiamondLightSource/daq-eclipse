f8c09fd7bede423b2b503f51abc6e861a601c68d
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-10-28 00:45:21 +0100

    Initial implementation of QueueRequest system
    Adds method for getting jobQueueID via the QueueControllerService. More complete implementation of QueueRequest bean.
    
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

--- org.eclipse.scanning.api/src/org/eclipse/scanning/api/event/queues/remote/QueueRequest.java
+++ org.eclipse.scanning.api/src/org/eclipse/scanning/api/event/queues/remote/QueueRequest.java
@@ -3,28 +3,98 @@ package org.eclipse.scanning.api.event.queues.remote;
 import org.eclipse.scanning.api.event.IdBean;
 import org.eclipse.scanning.api.event.status.Status;
 
+/**
+ * A QueueRequest contains a request for data about the {@link IQueueService} 
+ * from the client. From the {@link QueueRequestType}, the 
+ * {@link QueueResponseProcess} decides what data it will populate to fulfil 
+ * the request, before returning it to the client.
+ * 
+ * @author Michael Wharmby
+ *
+ */
 public class QueueRequest extends IdBean {
-	
+
+	/**
+	 * Version ID for serialization. Should be updated when class changed. 
+	 */
+	private static final long serialVersionUID = 20161027L;
+
+	private QueueRequestType requestType;
+
+	private String beanID;
+	private String queueID;
+	private String jobQueueID;
+	private Status beanStatus;
+
+	public QueueRequestType getRequestType() {
+		return requestType;
+	}
+	public void setRequestType(QueueRequestType requestType) {
+		this.requestType = requestType;
+	}
+	public String getBeanID() {
+		return beanID;
+	}
 	public void setBeanID(String beanID) {
-		//TODO
+		this.beanID = beanID;
 	}
-	
+	public String getQueueID() {
+		return queueID;
+	}
 	public void setQueueID(String queueID) {
-		//TODO
+		this.queueID = queueID;
 	}
-	
-	public void setType(QueueRequestType type) {
-		//TODO
-	}
-	
-	public Status getBeanStatus() {
-		//TODO
-		return null;
-	}
-	
 	public String getJobQueueID() {
-		//TODO
-		return null;
+		return jobQueueID;
+	}
+	public void setJobQueueID(String jobQueueID) {
+		this.jobQueueID = jobQueueID;
+	}
+	public Status getBeanStatus() {
+		return beanStatus;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((beanID == null) ? 0 : beanID.hashCode());
+		result = prime * result + ((beanStatus == null) ? 0 : beanStatus.hashCode());
+		result = prime * result + ((jobQueueID == null) ? 0 : jobQueueID.hashCode());
+		result = prime * result + ((queueID == null) ? 0 : queueID.hashCode());
+		result = prime * result + ((requestType == null) ? 0 : requestType.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		QueueRequest other = (QueueRequest) obj;
+		if (beanID == null) {
+			if (other.beanID != null)
+				return false;
+		} else if (!beanID.equals(other.beanID))
+			return false;
+		if (beanStatus != other.beanStatus)
+			return false;
+		if (jobQueueID == null) {
+			if (other.jobQueueID != null)
+				return false;
+		} else if (!jobQueueID.equals(other.jobQueueID))
+			return false;
+		if (queueID == null) {
+			if (other.queueID != null)
+				return false;
+		} else if (!queueID.equals(other.queueID))
+			return false;
+		if (requestType != other.requestType)
+			return false;
+		return true;
 	}
 
 }
--- org.eclipse.scanning.event.queues/src/org/eclipse/scanning/event/queues/remote/QueueResponseProcess.java
+++ org.eclipse.scanning.event.queues/src/org/eclipse/scanning/event/queues/remote/QueueResponseProcess.java
@@ -3,8 +3,10 @@ package org.eclipse.scanning.event.queues.remote;
 import org.eclipse.scanning.api.event.EventException;
 import org.eclipse.scanning.api.event.core.IPublisher;
 import org.eclipse.scanning.api.event.core.IResponseProcess;
+import org.eclipse.scanning.api.event.queues.IQueueControllerService;
 import org.eclipse.scanning.api.event.queues.remote.QueueRequest;
 import org.eclipse.scanning.api.event.queues.remote.QueueRequestType;
+import org.eclipse.scanning.event.queues.ServicesHolder;
 
 /**
  * Process to provide remote access to data stored in the 
@@ -21,9 +23,13 @@ public class QueueResponseProcess implements IResponseProcess<QueueRequest> {
 	private final QueueRequest requestBean;
 	private final IPublisher<QueueRequest> reponseBroadcaster;
 	
+	private final IQueueControllerService queueControl;
+	
 	public QueueResponseProcess(QueueRequest requestBean, IPublisher<QueueRequest> reponseBroadcaster) {
 		this.requestBean = requestBean;
 		this.reponseBroadcaster = reponseBroadcaster;
+		
+		queueControl = ServicesHolder.getQueueControllerService();
 	}
 
 	@Override
@@ -38,8 +44,28 @@ public class QueueResponseProcess implements IResponseProcess<QueueRequest> {
 
 	@Override
 	public QueueRequest process(QueueRequest request) throws EventException {
-		// TODO Auto-generated method stub
+		switch (requestBean.getRequestType()) {
+		case BEAN_STATUS:	return getBeanStatus();
+		case JOB_QUEUE_ID:	return getJobQueueID();
+		default: throw new EventException("Unknown QueueRequestType");
+		}
+	}
+	
+	private QueueRequest getBeanStatus() {
 		return null;
+		
+	}
+	
+	/**
+	 * Gets the job-queue ID of the {@link IQueueService} through the 
+	 * {@link IQueueControllerService}.
+	 * 
+	 * @return {@link QueueRequest} with jobQueueID field populated.
+	 */
+	private QueueRequest getJobQueueID() {
+		String jobQueueID = queueControl.getJobQueueID();
+		requestBean.setJobQueueID(jobQueueID);
+		return requestBean;
 	}
 
 }
