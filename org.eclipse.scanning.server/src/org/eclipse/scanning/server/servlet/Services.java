a7bc5166f2c86a3bf6c61fe31fa92e3a93636991
Matthew Gerring <matthew.gerring@gmail.com>
2016-11-10 15:11:29 +0000

    Enabled validation when trying to run a ScanRequest.
    http://jira.diamond.ac.uk/browse/DAQ-358

--- org.eclipse.scanning.server/src/org/eclipse/scanning/server/servlet/Services.java
+++ org.eclipse.scanning.server/src/org/eclipse/scanning/server/servlet/Services.java
@@ -5,6 +5,7 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 import org.eclipse.scanning.api.device.IScannableDeviceService;
+import org.eclipse.scanning.api.IValidatorService;
 import org.eclipse.scanning.api.device.IRunnableDeviceService;
 import org.eclipse.scanning.api.event.IEventService;
 import org.eclipse.scanning.api.event.IMessagingService;
@@ -33,6 +34,7 @@ public class Services {
 	private static IFilePathService        filePathService;
 	private static IScriptService          scriptService;
 	private static IMessagingService       messagingService;
+	private static IValidatorService       validatorService;
 
 	private static final Set<IPreprocessor> preprocessors = new LinkedHashSet<>();
 
@@ -108,7 +110,15 @@ public class Services {
 		return messagingService;
 	}
 
-	public static void setMessagingService(IMessagingService messagingService) {
+	public void setMessagingService(IMessagingService messagingService) {
 		Services.messagingService = messagingService;
+	}
+
+	public static IValidatorService getValidatorService() {
+		return validatorService;
+	}
+
+	public void setValidatorService(IValidatorService validatorService) {
+		Services.validatorService = validatorService;
 	}
 }
