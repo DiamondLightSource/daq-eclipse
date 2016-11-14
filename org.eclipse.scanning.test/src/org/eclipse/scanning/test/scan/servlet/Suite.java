e3451653e64d60ae6874faef808bbb02cb0fdafb
Martin Gaughran <martin.gaughran@diamond.ac.uk>
2016-11-14 16:10:40 +0000

    Added ScanProcessTest to tests run by travis.

---
+++ org.eclipse.scanning.test/src/org/eclipse/scanning/test/scan/servlet/Suite.java
@@ -0,0 +1,13 @@
+package org.eclipse.scanning.test.scan.servlet;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(org.junit.runners.Suite.class)
+@SuiteClasses({
+
+	ScanProcessTest.class,
+
+})
+public class Suite {
+}
