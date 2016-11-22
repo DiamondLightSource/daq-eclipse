16162168c2c74db5ec5c44db528bec203a5ff3f0
Matthew Gerring <matthew.gerring@gmail.com>
2016-11-22 14:33:47 +0000

    Remove "python.import.site" set to false

--- org.eclipse.scanning.points/src/org/eclipse/scanning/points/ScanPointGeneratorFactory.java
+++ org.eclipse.scanning.points/src/org/eclipse/scanning/points/ScanPointGeneratorFactory.java
@@ -247,7 +247,7 @@ public class ScanPointGeneratorFactory {
 	    	props.put("python.home", jythonDir.getAbsolutePath());
 	    	props.put("python.console.encoding", "UTF-8"); // Used to prevent: console: Failed to install '': java.nio.charset.UnsupportedCharsetException: cp0.
 	    	props.put("python.security.respectJavaAccessibility", "false"); //don't respect java accessibility, so that we can access protected members on subclasses
-	    	props.put("python.import.site","false");
+	    	//props.put("python.import.site","false");
 
 	    	Properties preprops = System.getProperties();
 	    	
