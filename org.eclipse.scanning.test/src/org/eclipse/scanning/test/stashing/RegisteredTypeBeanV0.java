891e9a7e707520b81c434a1f5fadcba7d10ee40e
Matthew Gerring <matthew.gerring@gmail.com>
2017-01-05 12:02:47 +0000

    Improved test

--- org.eclipse.scanning.test/src/org/eclipse/scanning/test/stashing/RegisteredTypeBeanV0.java
+++ org.eclipse.scanning.test/src/org/eclipse/scanning/test/stashing/RegisteredTypeBeanV0.java
@@ -1,15 +1,13 @@
 package org.eclipse.scanning.test.stashing;
 
-public class SpecialTypeBean {
+public class RegisteredTypeBeanV0 {
 
 	private int value1;
 	private String value2;
-	private double value3;
 	
-	public SpecialTypeBean() {
+	public RegisteredTypeBeanV0() {
 		value1 = 1;
 		value2 = "2";
-		value3 = 3.0d;
 	}
 	
 	public int getValue1() {
@@ -24,21 +22,12 @@ public class SpecialTypeBean {
 	public void setValue2(String value2) {
 		this.value2 = value2;
 	}
-	public double getValue3() {
-		return value3;
-	}
-	public void setValue3(double value3) {
-		this.value3 = value3;
-	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + value1;
 		result = prime * result + ((value2 == null) ? 0 : value2.hashCode());
-		long temp;
-		temp = Double.doubleToLongBits(value3);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
 		return result;
 	}
 	@Override
@@ -49,15 +38,13 @@ public class SpecialTypeBean {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		SpecialTypeBean other = (SpecialTypeBean) obj;
+		RegisteredTypeBeanV0 other = (RegisteredTypeBeanV0) obj;
 		if (value1 != other.value1)
 			return false;
 		if (value2 == null) {
 			if (other.value2 != null)
 				return false;
 		} else if (!value2.equals(other.value2))
-			return false;
-		if (Double.doubleToLongBits(value3) != Double.doubleToLongBits(other.value3))
 			return false;
 		return true;
 	}
