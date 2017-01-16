93823fbc4053ea0a720fa3144cc54823db419578
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2017-01-16 12:01:48 +0000

    Updates child queue infrastructure
    Changes AtomQueueProcessor & QueueListener to use the new QueueProcess
    structure rather than QueueProcess + QueueProcessor. Adds testing for
    status of active queue to decide whether to call stop during tidy.
    
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

--- org.eclipse.scanning.event.queues/src/org/eclipse/scanning/event/queues/processors/AtomQueueProcessor.java
+++ org.eclipse.scanning.event.queues/src/org/eclipse/scanning/event/queues/processors/AtomQueueProcessor.java
@@ -1,14 +1,19 @@
 package org.eclipse.scanning.event.queues.processors;
 
 import org.eclipse.scanning.api.event.EventException;
+import org.eclipse.scanning.api.event.IEventService;
+import org.eclipse.scanning.api.event.core.IConsumer;
 import org.eclipse.scanning.api.event.core.ISubscriber;
+import org.eclipse.scanning.api.event.queues.IQueue;
 import org.eclipse.scanning.api.event.queues.IQueueControllerService;
-import org.eclipse.scanning.api.event.queues.IQueueProcessor;
+import org.eclipse.scanning.api.event.queues.IQueueProcess;
 import org.eclipse.scanning.api.event.queues.IQueueService;
 import org.eclipse.scanning.api.event.queues.beans.IHasAtomQueue;
 import org.eclipse.scanning.api.event.queues.beans.QueueAtom;
 import org.eclipse.scanning.api.event.queues.beans.Queueable;
 import org.eclipse.scanning.api.event.status.Status;
+import org.eclipse.scanning.event.queues.Queue;
+import org.eclipse.scanning.event.queues.QueueProcess;
 import org.eclipse.scanning.event.queues.ServicesHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,7 +30,9 @@ import org.slf4j.LoggerFactory;
  *            {@link IHasAtomQueue}.
  * @param <Q> Bean from within the AtomQueue - implements {@link QueueAtom}.
  */
-public class AtomQueueProcessor<P extends Queueable & IHasAtomQueue<Q>, Q extends QueueAtom> {
+public class AtomQueueProcessor<P extends Queueable & IHasAtomQueue<Q>, 
+								Q extends QueueAtom, 
+								T extends Queueable> {
 	
 	private static Logger logger = LoggerFactory.getLogger(AtomQueueProcessor.class);
 	
@@ -34,7 +41,7 @@ public class AtomQueueProcessor<P extends Queueable & IHasAtomQueue<Q>, Q extend
 	private QueueListener<P, Q> queueListener;
 	private ISubscriber<QueueListener<P, Q>> queueSubscriber;
 	
-	private IQueueProcessor<P> parentProcessor;
+	private QueueProcess<P, T> parentProcess;
 	private String activeQueueID; 
 	
 	/**
@@ -42,14 +49,13 @@ public class AtomQueueProcessor<P extends Queueable & IHasAtomQueue<Q>, Q extend
 	 * {@link IQueueService} and {@link IQueueControllerService} provided by 
 	 * the OSGi services holder ({@link ServicesHolder}).
 	 * 
-	 * @param parentProcessor {@link IQueueProcessor} associated with the 
-	 *        {@link IQueueProcess} tasked with processing a bean implementing 
-	 *        {@link IHasAtomQueue}.
+	 * @param parentProcess {@link IQueueProcess} tasked with processing a 
+	 * 						bean implementing {@link IHasAtomQueue}.
 	 */
-	public AtomQueueProcessor(IQueueProcessor<P> parentProcessor) {
+	public AtomQueueProcessor(QueueProcess<P, T> parentProcess) {
 		queueService = ServicesHolder.getQueueService();
 		queueController = ServicesHolder.getQueueControllerService();
-		this.parentProcessor = parentProcessor;
+		this.parentProcess = parentProcess;
 	}
 	
 	/**
@@ -66,10 +72,10 @@ public class AtomQueueProcessor<P extends Queueable & IHasAtomQueue<Q>, Q extend
 	 */
 	public void run() throws EventException, InterruptedException {
 		//Everything should be set up by now, so we can get the atomQueue
-		final P atomQueue = parentProcessor.getProcessBean();
+		final P atomQueue = parentProcess.getQueueBean();
 		
 		//Create a new active queue to submit the atoms into
-		parentProcessor.getQueueBroadcaster().broadcast(Status.RUNNING, 0d, "Registering new active queue.");
+		parentProcess.broadcast(Status.RUNNING, 0d, "Registering new active queue.");
 		activeQueueID = queueService.registerNewActiveQueue();
 		
 		/*
@@ -78,9 +84,9 @@ public class AtomQueueProcessor<P extends Queueable & IHasAtomQueue<Q>, Q extend
 		 * has to listen for. 
 		 */
 		queueListener = new QueueListener<>(
-				parentProcessor.getQueueBroadcaster(), 
-				parentProcessor.getProcessBean(), 
-				parentProcessor.getProcessorLatch());
+				parentProcess, 
+				parentProcess.getQueueBean(), 
+				parentProcess.getProcessLatch());
 		queueSubscriber = queueController.createQueueSubscriber(activeQueueID);
 		queueSubscriber.addListener(queueListener);
 		
@@ -88,8 +94,8 @@ public class AtomQueueProcessor<P extends Queueable & IHasAtomQueue<Q>, Q extend
 		 * Spool beans from bean atom queue to the queue service
 		 * (queue empty after this!)
 		 */
-		parentProcessor.getQueueBroadcaster().broadcast(Status.RUNNING, 1d, "Submitting atoms to active queue.");
-		Queueable parentBean = parentProcessor.getProcessBean();//TODO This is not needed - see atomQueue
+		parentProcess.broadcast(Status.RUNNING, 1d, "Submitting atoms to active queue.");
+		Queueable parentBean = parentProcess.getQueueBean();//TODO This is not needed - see atomQueue
 		while (atomQueue.atomQueueSize() > 0) {
 			QueueAtom nextAtom = atomQueue.viewNextAtom();
 			if (nextAtom.getBeamline() != parentBean.getBeamline()) {
@@ -108,10 +114,10 @@ public class AtomQueueProcessor<P extends Queueable & IHasAtomQueue<Q>, Q extend
 		 * Start processing & wait for it to end - after returning, we start 
 		 * the post-match analysis immediately.
 		 */
-		parentProcessor.getQueueBroadcaster().broadcast(Status.RUNNING, 4d, "Beans submitted. Starting active queue...");
+		parentProcess.broadcast(Status.RUNNING, 4d, "Beans submitted. Starting active queue...");
 		queueService.startActiveQueue(activeQueueID);
-		parentProcessor.getQueueBroadcaster().broadcast(Status.RUNNING, 5d, "Waiting for active queue to complete");
-		parentProcessor.getProcessorLatch().await();
+		parentProcess.broadcast(Status.RUNNING, 5d, "Waiting for active queue to complete");
+		parentProcess.getProcessLatch().await();
 	}
 	
 	/**
@@ -138,8 +144,11 @@ public class AtomQueueProcessor<P extends Queueable & IHasAtomQueue<Q>, Q extend
 		
 		//Tidy up our processes, before handing back control
 		try {
-		queueService.stopActiveQueue(activeQueueID, false);
-		queueService.deRegisterActiveQueue(activeQueueID);
+			//Only stop the queue if it hasn't already been
+			if (queueService.getQueue(activeQueueID).getStatus().isActive()) {
+				queueService.stopActiveQueue(activeQueueID, false);
+			}
+			queueService.deRegisterActiveQueue(activeQueueID);
 		} catch (EventException evEx) {
 			/*
 			 * If the queueService pushes back an EventException with the 
