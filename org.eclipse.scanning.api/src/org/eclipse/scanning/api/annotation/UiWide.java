30ad40e91e19cd4ca84ea39d5d34557e58630f67
Matthew Gerring <matthew.gerring@gmail.com>
2017-02-16 16:02:58 +0000

    Added Eclipse headers.

--- org.eclipse.scanning.api/src/org/eclipse/scanning/api/annotation/UiWide.java
+++ org.eclipse.scanning.api/src/org/eclipse/scanning/api/annotation/UiWide.java
@@ -1,4 +1,14 @@
-// Metawidget
+/*-
+ *******************************************************************************
+ * Copyright (c) 2011, 2016 Diamond Light Source Ltd.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Matthew Gerring - initial API and implementation and/or initial documentation
+ *******************************************************************************/
 //
 // For historical reasons, this file is licensed under the LGPL
 // (http://www.gnu.org/licenses/lgpl-2.1.html).
